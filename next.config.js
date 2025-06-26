7442f3439a91e9fa7babbc91bb76389bdfa1c77d
Chinyere Unamba <cunamba2@gmail.com>
2025-06-26 20:26:18 +0100

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -8,37 +8,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-  optimizeFonts: false, // disables automatic font fetching
-},
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        bufferutil: false,
-        "utf-8-validate": false,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-
-    config.ignoreWarnings = [
-      {
-        module: /@supabase\/realtime-js/,
-        message:
-          /Critical dependency: the request of a dependency is an expression/,
-      },
-    ];
-
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^(bufferutil|utf-8-validate)$/,
-      })
-    );
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
