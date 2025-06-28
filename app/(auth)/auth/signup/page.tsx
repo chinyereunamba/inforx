0c036a0ca707f309044ff65ede37178b9e0dbc38
Chinyere Unamba <cunamba2@gmail.com>
2025-06-28 23:56:28 +0100

    Updated page.tsx

--- app/(auth)/auth/signup/page.tsx
+++ app/(auth)/auth/signup/page.tsx
@@ -239,7 +239,7 @@ export default function SignUpPage() {
       </form>
 
       {/* Divider */}
-      <div className="relative">
+      <div className="relative space-y-4">
         <div className="absolute inset-0 flex items-center">
           <div className="w-full border-t border-slate-300" />
         </div>
