ca835369fcd85bee617ad57ba42fc264d535fcb6
Chinyere Unamba <cunamba2@gmail.com>
2025-06-28 17:00:07 +0100

    Fix Demo Button Interaction

--- components/demo/DemoPage.tsx
+++ components/demo/DemoPage.tsx
@@ -72,7 +72,7 @@ export default function DemoPage() {
       
       return () => ctx.revert();
     }
-  }, []);
+  }, [isDemoMode]);
 
   const startInteractiveDemo = () => {
     setIsDemoMode(true);
